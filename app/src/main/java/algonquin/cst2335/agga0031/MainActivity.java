65b4ac7c5f3a76b77ab2f16889803de01c3d5a61
Mannan Aggarwal <agga0031@algonquinlive.com>
2023-01-25 15:57:35 -0500

    Sixth Commit: setOnClickListener Initialised using Lambda Function.

--- app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.agga0031;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button btn = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.edittext);
+        String editString = myedit.getText().toString();
+       // mytext.setText( "Your edit text has: " + editString);
+
+        btn.setOnClickListener( v -> mytext.setText("Your String has: "+ editString));
     }
 }
