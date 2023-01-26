bc379f972581283e24e34e32b35f8a64116aa6fa
Mannan Aggarwal <agga0031@algonquinlive.com>
2023-01-25 19:22:55 -0500

    Seventh Commit:Added View Model.

--- app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
@@ -1,24 +1,46 @@
 package algonquin.cst2335.agga0031;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.agga0031.data.MainActivityViewModel;
+import algonquin.cst2335.agga0031.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        binding = ActivityMainBinding.inflate( getLayoutInflater() ); //this will the layout.
+
+        binding.textview.setText(viewModel.editTextContents);
+
+        setContentView(binding.getRoot()); //this load stuff on the screen.
+        /*
         TextView mytext = findViewById(R.id.textview);
         Button btn = findViewById(R.id.mybutton);
         EditText myedit = findViewById(R.id.edittext);
         String editString = myedit.getText().toString();
+        */
+
        // mytext.setText( "Your edit text has: " + editString);
 
-        btn.setOnClickListener( v -> mytext.setText("Your String has: "+ editString));
+        binding.mybutton.setOnClickListener( v -> {
+            viewModel.editTextContents = ("Your String has: "+ binding.edittext.getText().toString());
+
+            binding.textview.setText(viewModel.editTextContents);
+
+        });
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/agga0031/data/MainActivityViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.agga0031.data;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel{
+
+    public String editTextContents;
+
+}
