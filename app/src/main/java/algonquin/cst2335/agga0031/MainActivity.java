c9b9289ec84e33fde227b3ab6df8419e4647b6e9
Mannan Aggarwal <agga0031@algonquinlive.com>
2023-01-25 21:35:05 -0500

    Eighth Commit: Finished Week 2

--- app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0031/MainActivity.java
@@ -3,10 +3,12 @@ package algonquin.cst2335.agga0031;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.agga0031.data.MainActivityViewModel;
 import algonquin.cst2335.agga0031.databinding.ActivityMainBinding;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate( getLayoutInflater() ); //this will the layout.
 
-        binding.textview.setText(viewModel.editTextContents);
+        //binding.textview.setText(viewModel.editTextContents);
 
         setContentView(binding.getRoot()); //this load stuff on the screen.
         /*
@@ -37,10 +39,55 @@ public class MainActivity extends AppCompatActivity {
        // mytext.setText( "Your edit text has: " + editString);
 
         binding.mybutton.setOnClickListener( v -> {
-            viewModel.editTextContents = ("Your String has: "+ binding.edittext.getText().toString());
+            viewModel.editTextContents.postValue(binding.edittext.getText().toString());
 
-            binding.textview.setText(viewModel.editTextContents);
+            //viewModel.editTextContents = ("Your String has: "+ binding.edittext.getText().toString());
 
+            //binding.textview.setText(viewModel.editTextContents);
+
+        });
+
+        viewModel.editTextContents.observe(this, s -> {
+            binding.textview.setText("Your String has: "+ binding.edittext.getText().toString());
+        });
+
+
+        viewModel.isSelected.observe( this , selected->{
+            binding.checkBox.setChecked(selected);
+            binding.radioButton.setChecked(selected);
+            binding.switch1.setChecked(selected);
+            Context context = getApplicationContext();
+            CharSequence text = "The value is now: " + binding.checkBox.isChecked();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+
+        binding.checkBox.setOnCheckedChangeListener((checkBox , isChecked) -> {
+            viewModel.isSelected.postValue(isChecked);
+        });
+
+        binding.radioButton.setOnCheckedChangeListener((radioButton , isChecked) -> {
+            viewModel.isSelected.postValue(isChecked);
+        });
+
+        binding.switch1.setOnCheckedChangeListener((switch1 , isChecked) -> {
+            viewModel.isSelected.postValue(isChecked);
+        });
+
+
+        binding.imageButton.setOnClickListener(click -> {
+            viewModel.iButton.postValue(binding.imageButton.getHeight() + "*" + binding.imageButton.getWidth());
+        });
+
+        viewModel.iButton.observe(this, c -> {
+            Context context = getApplicationContext();
+            CharSequence text = "The value is now: " + binding.imageButton.getHeight() + "*" + binding.imageButton.getWidth();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         });
     }
 }
--- app/src/main/java/algonquin/cst2335/agga0031/data/MainActivityViewModel.java
+++ app/src/main/java/algonquin/cst2335/agga0031/data/MainActivityViewModel.java
@@ -1,9 +1,11 @@
 package algonquin.cst2335.agga0031.data;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel{
 
-    public String editTextContents;
-
+    public MutableLiveData<String> editTextContents = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
+    public MutableLiveData<String> iButton = new MutableLiveData<>();
 }
